a68b4acbc43b60e9d02f5c6235f0f5439df4ef04
Adam Oller <81510507+Adamocho@users.noreply.github.com>
2022-06-06 19:30:37 +0200

    Finish congigure.sh script

---
+++ bootstrap/configure.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+SCRIPT_PATH=$(dirname "$0")
+MODULES=$( ls "$SCRIPT_PATH/../etc" | xargs )
+
+for MODULE in $MODULES
+do
+    read -p "Do you want to install $MODULE? [Y/n] " ANSWER
+
+    [ "$ANSWER" = y ] || [ -z "$ANSWER" ] && {
+        cp -ufr "$SCRIPT_PATH/../etc/$MODULE" ~/.config && {
+            printf "\033[92mDONE\033[00m: config installed\n"
+        } || {
+            printf "\033[91mERROR\033[00m: installation failed\n"
+        }
+    } || {
+        printf "\033[93mNOTE\033[00m: config omitted\n"
+    }
+done
