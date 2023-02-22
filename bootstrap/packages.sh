4157f563abd56ae9ace229d19d5af53b74c733ee
Adamocho <81510507+Adamocho@users.noreply.github.com>
2023-02-22 21:13:44 +0100

    Fix 6a440f3d9546: Patch path bug; Insert $HOME instead of '~'; Remove -print0 option from find

--- bootstrap/packages.sh
+++ bootstrap/packages.sh
@@ -2,8 +2,8 @@
 
 SCRIPT_PATH=$(dirname "$0")
 PACKAGES_PATH="${SCRIPT_PATH}/../software/packages"
-PACKAGES_FILES=$( find "${PACKAGES_PATH}" -maxdepth 1 -type f -print0 | xargs )
-OUTPUT_FILE="~/.config/packages_list.txt"
+PACKAGES_FILES=$( find "${PACKAGES_PATH}" -maxdepth 1 -type f | xargs )
+OUTPUT_FILE="${HOME}/.config/packages_list.txt"
 
 printf "" > "${OUTPUT_FILE}" 
 
@@ -12,7 +12,7 @@ do
     read -r -p "Do you want to iterate through ${file}? [Y/n] "
 
     [ "${REPLY}" = y ] || [ -z "${REPLY}" ] && {
-        PACKAGES=$(grep -E "^[a-zA-Z0-9_\-]+" "${PACKAGES_PATH}/${file}")
+        PACKAGES=$(grep -E "^[a-zA-Z0-9_\-]+" "${file}")
 
         for package in $PACKAGES
         do
