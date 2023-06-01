3362f3983477c0df5628fa2983679d210f7ac5d9
Adamocho <81510507+Adamocho@users.noreply.github.com>
2023-06-01 19:14:18 +0200

    Add a dmenu script for managing wallpaper

---
+++ bin/dmenu_wallpaper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+WALLPAPER_DIR="$HOME/.config/wallpaper"
+
+cd "$WALLPAPER_DIR" || exit
+
+OPTIONS=$(find . -maxdepth 2 -type f)
+RESULT=$(echo "$OPTIONS" | dmenu -i -p "Select wallpaper: ")
+
+feh --no-fehbg --bg-scale "$WALLPAPER_DIR/$RESULT"
