9ce3a2c75f84dedd7b2ed4da0303602da728a925
Adamocho <81510507+Adamocho@users.noreply.github.com>
2023-06-01 19:29:43 +0200

    Fix PATH bug in scripts.sh

--- bootstrap/scripts.sh
+++ bootstrap/scripts.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 SCRIPT_PATH=$(dirname "$0")
-SCRIPTS=$( find "${SCRIPT_PATH}/../bin/*" -printf "%f " | xargs )
-OUTPUT_DIR="~/.local/bin"
+SCRIPTS=$( find "${SCRIPT_PATH}/../bin/" -maxdepth 1 -type f)
+OUTPUT_DIR="$HOME/.local/bin"
 
 for SCRIPT in $SCRIPTS
 do
-    cp -uiv "${SCRIPT_PATH}/../bin/${SCRIPT}" "${OUTPUT_DIR}" && {
+    cp -uiv "${SCRIPT}" "${OUTPUT_DIR}" && {
         printf "\033[92mDONE\033[00m: script installed\n"
     } || {
         printf "\033[91mERROR\033[00m: installation failed\n"
