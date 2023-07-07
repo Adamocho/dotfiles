6ff4a55f1443721c7218f8fb6a4a64221da06aa0
Adamocho <81510507+Adamocho@users.noreply.github.com>
2023-07-07 17:45:18 +0200

    Rewrite install.sh script: - Now it is cleaner - Works better - Shellcheck approved :D

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -3,32 +3,32 @@
 SCRIPT_PATH=$( realpath "$0" | xargs dirname )
 CONFIG_PATH="${SCRIPT_PATH}/../etc"
 BACKUP_DIR="${HOME}/.config/dotfiles_backup"
+TMP_DIR="/tmp"
 
 LOCAL_DOTFILES=$( find "${CONFIG_PATH}" -maxdepth 2 -type f -name ".*" -printf "%f\n" )
 GLOBAL_DOTFILES=$( find "${HOME}" -maxdepth 1 -type f -name ".*" -printf "%f\n" )
-GLOBAL_DOTLINKS=$( find "${HOME}" -maxdepth 1 -type l -name ".*" -printf "%f\n" )
+# Links could be used in the future
+# GLOBAL_DOTLINKS=$( find "${HOME}" -maxdepth 1 -type l -name ".*" -printf "%f\n" )
 
+# Create backup directory if doesn't exist
 [[ -d "${BACKUP_DIR}" ]] || { mkdir "${BACKUP_DIR}"; }
 
-for global_file in $GLOBAL_DOTFILES
+# Create temporary files for comparing (diff) local vs global results
+TMP_FILE_LOCAL=$( mktemp tmp.XXXX -p "${TMP_DIR}")
+TMP_FILE_GLOBAL=$( mktemp tmp.XXXX -p "${TMP_DIR}")
+
+echo "$LOCAL_DOTFILES" | sort > "$TMP_FILE_LOCAL"
+echo "$GLOBAL_DOTFILES" | sort > "$TMP_FILE_GLOBAL"
+
+SWAPPABLE_FILES=$( comm -1 -2 "$TMP_FILE_LOCAL" "$TMP_FILE_GLOBAL" )
+
+for file in $LOCAL_DOTFILES
 do
-    for local_dotfile in $LOCAL_DOTFILES
-    do
-        [[ "${local_dotfile}" = "${global_file}" ]] && {
-            echo "FOUND FILE MATCH"
-
-            LINKFILE=$( find "${CONFIG_PATH}" -maxdepth 2 -type f -name "${local_dotfile}" | head -n 1 )
-
-            mv -iuv "${HOME}/${global_file}" "${BACKUP_DIR}"
-
-            ln -siv "${LINKFILE}" "${HOME}/${global_file}"
-        } 
-    done
-done
-
-for local_dotfile in $LOCAL_DOTFILES
-do
-    LINKFILE=$( find "${CONFIG_PATH}" -maxdepth 2 -type f -name "${local_dotfile}" | head -n 1 )
-
-    ln -siv "${LINKFILE}" "${HOME}/${local_dotfile}"
+    FOUND=$( echo "$SWAPPABLE_FILES" | grep -xc "$file" )
+    [[ "${FOUND}" = 1 ]] && {
+        printf "FOUND FILE MATCH\n"
+        mv -iv "${HOME}/${file}" "${BACKUP_DIR}"
+    } 
+        LINKFILE=$( find "${CONFIG_PATH}" -maxdepth 2 -type f -name "${file}" | head -n 1 )
+        ln -siv "${LINKFILE}" "${HOME}/${file}"
 done
