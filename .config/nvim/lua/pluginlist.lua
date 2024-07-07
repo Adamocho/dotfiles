d2645885023e3d7efb445f06195871d834b5a9e3
Adamocho <81510507+Adamocho@users.noreply.github.com>
2024-07-07 08:36:11 +0200

    Neovim - add missing neovim-icons package to lualine and enable icons. River init - add swaylock.

--- .config/nvim/lua/pluginlist.lua
+++ .config/nvim/lua/pluginlist.lua
@@ -1,65 +1,71 @@
 -- List of plugins used, returning an array
 return {
-	{ -- Commenting
-		"numToStr/Comment.nvim",
-		config = function ()
-			require("Comment").setup()
-		end
-	},
-	{ -- Theme
-		"rose-pine/neovim",
-		name = "rose-pine",
-		priority = 1000,
-		config = function()
-			vim.cmd("colorscheme rose-pine")
-		end
-	},
-	{ -- Custom line
-		"nvim-lualine/lualine.nvim",
-		config = function ()
-			require("lualine").setup({
-				theme = 'rose-pine',
-			})
-		end,
-	},
+    { -- Commenting
+        "numToStr/Comment.nvim",
+        config = function ()
+            require("Comment").setup()
+        end
+    },
 
-	-- Some additional tools
+    { -- Theme
+        "rose-pine/neovim",
+        name = "rose-pine",
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme rose-pine")
+        end
+    },
 
-	-- slow telescope
+    { -- Custom line
+        "nvim-lualine/lualine.nvim",
+        dependencies = {
+            'nvim-tree/nvim-web-devicons'
+        },
+        config = function ()
+            require("lualine").setup({
+                icons_enabled = true,
+                theme = 'rose-pine',
+            })
+        end,
+    },
+
+    -- Some additional tools
+
+    -- slow telescope
     "nvim-telescope/telescope.nvim",
-	-- fast telescope
-	-- {
-	-- 	"nvim-telescope/telescope-fzf-native.nvim",
-	-- 	build = "make",
-	-- },
+    -- fast telescope
+    -- {
+    -- 	"nvim-telescope/telescope-fzf-native.nvim",
+    -- 	build = "make",
+    -- },
 
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		performance = {
-			reset_packpath = false,
-			rtp = { reset = false }
-		}
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        performance = {
+            reset_packpath = false,
+            rtp = { reset = false }
+        }
+    },
 
     "mbbill/undotree",
 
-	-- LSP
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	"neovim/nvim-lspconfig",
-	"folke/neodev.nvim",
+    -- LSP
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    "folke/neodev.nvim",
 
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
 
-			-- additional code snippets
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
+            -- additional code snippets
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
             "rafamadriz/friendly-snippets"
-		},
-	},
+        },
+    },
 }
