5bd23c5d5b139f85c7dd52765cc4cb7708c16fc0
Adamocho <81510507+Adamocho@users.noreply.github.com>
2024-09-18 18:57:24 +0200

    Update nvim config

--- .config/nvim/lua/pluginlist.lua
+++ .config/nvim/lua/pluginlist.lua
@@ -63,7 +63,7 @@ return {
             "hrsh7th/cmp-nvim-lsp",
 
             -- additional code snippets
-            "L3MON4D3/LuaSnip",
+            "L3MON4D3/LuaSnip", 
             "saadparwaiz1/cmp_luasnip",
             "rafamadriz/friendly-snippets"
         },
