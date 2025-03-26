bee574a96151fd235636f31cad907752c23087d4
Adamocho <81510507+Adamocho@users.noreply.github.com>
2025-03-26 20:06:24 +0100

    Disable isfname:append because it was causing errors

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -36,7 +36,7 @@ vim.opt.smartcase = true
 vim.o.scrolloff = 8
 vim.o.signcolumn = "yes"
 -- For filenames that include an at `@` character
-vim.o.isfname:append("@-@")
+-- vim.o.isfname:append("@-@")
 
 -- Smooth as butter
 vim.o.updatetime = 300
