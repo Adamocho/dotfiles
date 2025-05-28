7077e1d1fc26479e86a7ae5ff81f54afa684a43e
Adamocho <81510507+Adamocho@users.noreply.github.com>
2025-05-28 23:42:52 +0200

    Add basic harpoon setup just for testing purposes

--- .config/nvim/lua/pluginlist.lua
+++ .config/nvim/lua/pluginlist.lua
@@ -104,4 +104,13 @@ return {
         }
       end
     },
+
+    {
+      "ThePrimeagen/harpoon",
+      branch = "harpoon2",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
+      }
+    },
 }
